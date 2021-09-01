7b54140d9831b020d803138dc0e8a383e95d413b
Enyxis <nookinx@gmail.com>
2021-09-01 19:27:23 -0300

    definir random da frutinha e size da cobrinha

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let food = {
 }
 
 function criarBG(){
-    context.fillStyle = "purple";
+    context.fillStyle = "black";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
@@ -57,8 +57,14 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
-
+    if(snakeX != food.x || snakeY != food.y){
+       snake.pop(); 
+    }
+    else{
+      food.x = Math.floor(Math.random() * 15 + 1) * box,
+      food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
+    
     let newHead = {
         x: snakeX,
         y: snakeY
